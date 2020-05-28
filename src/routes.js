c16346876a911dfbe742df1b583ee9a62aef52a8
Max Rafael <maxrafael@hotmail.com>
2020-05-28 09:02:38 -0300

    Commit test build

--- src/routes.js
+++ src/routes.js
@@ -18,8 +18,6 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.get('/', (req, res) => res.send('new route'));
-
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
